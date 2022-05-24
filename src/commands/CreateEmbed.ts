7e8f22ad0dcf92048bd3fa4e50eb2cbf6d9c1e0a
ssMMiles <github@milesmoonlove.com>
2022-05-24 15:48:28 +0100

    fix

--- src/commands/CreateEmbed.ts
+++ src/commands/CreateEmbed.ts
@@ -81,12 +81,12 @@ export class CreateEmbed implements ISlashCommand {
         } catch (err: any) {
           switch (err.code as number) {
             case 30007: {
-              await ctx.send(SimpleError(`You've reached the maximum number of webhooks.`).setEphemeral(true));
+              await ctx.edit(SimpleError(`You've reached the maximum number of webhooks.`).setEphemeral(true));
               break;
             }
             default: {
               console.error(err);
-              await ctx.send(SimpleError("An unknown error occurred.").setEphemeral(true));
+              await ctx.edit(SimpleError("An unknown error occurred.").setEphemeral(true));
               break;
             }
           }
