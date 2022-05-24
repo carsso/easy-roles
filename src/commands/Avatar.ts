56a6942c0af1b67925d075c07b2ec01987f0a617
ssMMiles <github@milesmoonlove.com>
2022-05-24 13:58:22 +0100

    add /avatar

--- src/commands/Avatar.ts
+++ src/commands/Avatar.ts
@@ -5,13 +5,75 @@ import {
   ButtonContext,
   ButtonStyle,
   EmbedBuilder,
+  ISlashCommand,
   IUserCommand,
   MessageBuilder,
+  SlashCommandBuilder,
+  SlashCommandContext,
+  SlashCommandUserOption,
   UserCommandBuilder,
   UserCommandContext
 } from "interactions.ts";
 
-export default class Avatar implements IUserCommand {
+export class Avatar implements ISlashCommand {
+  public builder = new SlashCommandBuilder("avatar", "Get a user's avatar.").addUserOption(
+    new SlashCommandUserOption("user", "The user whose avatar you'd like to fetch.").setRequired(true)
+  );
+
+  public handler = async (ctx: SlashCommandContext): Promise<void> => {
+    const target = ctx.getUserOption("avatar");
+
+    let server = false;
+
+    const userId = target.user.id;
+    const guildId = ctx.interaction.guild_id;
+
+    let name = target.user.username;
+    let avatar = target.user.avatar ?? "";
+
+    if (target.member) {
+      if (target.member.nick) {
+        name = target.member.nick;
+      }
+
+      if (target.member.avatar) {
+        avatar = target.member.avatar;
+        server = true;
+      }
+    }
+
+    const actionRow = new ActionRowBuilder([
+      await ctx.createComponent("user", {
+        name: target.user.username.length > 15 ? name.substring(0, 15) + "..." : name,
+        userId: userId,
+        avatar: target.user.avatar
+      })
+    ]);
+
+    let guildAvatarButton;
+
+    if (server) {
+      guildAvatarButton = await ctx.createComponent("guild", {
+        name: name.length > 15 ? name.substring(0, 15) + "..." : name,
+        userId: userId,
+        avatar: avatar,
+        guildId: guildId
+      });
+    } else {
+      guildAvatarButton = await ctx.createComponent("guild");
+
+      guildAvatarButton.setDisabled(true);
+    }
+
+    actionRow.addComponents(guildAvatarButton);
+
+    return ctx.reply(
+      (await buildAvatarEmbed(name, userId, avatar, server ? guildId : undefined)).addComponents(actionRow)
+    );
+  };
+}
+
+export class AvatarContext implements IUserCommand {
   public builder = new UserCommandBuilder("Avatar");
 
   public handler = async (ctx: UserCommandContext): Promise<void> => {
